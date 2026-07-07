dac2af3df8a2d142d6506d942e45b22562ecac4b
bbakernoaa <barry.baker@noaa.gov>
2026-07-07 10:27:07 -0400

    feat: migrate and link complete non-mocked UPP pipeline directly to Spack packages

--- cmake/Findcrtm.cmake
+++
@@ -1,8 +0,0 @@
-# cmake/Findcrtm.cmake
-# Intercepts find_package(crtm) and exposes an empty interface target since mock_crtm.f90 is compiled directly inside the upp target.
-
-if(NOT TARGET crtm::crtm)
-    add_library(crtm::crtm INTERFACE IMPORTED GLOBAL)
-    set(crtm_FOUND TRUE)
-    set(CRTM_FOUND TRUE)
-endif()
